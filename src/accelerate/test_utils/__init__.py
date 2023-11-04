f8165b3cf0b0f0ee78afdf45b3a44a8bf6adb262
statelesshz <hzji210@gmail.com>
2023-11-04 20:42:22 +0800

    device agnostic testing

--- src/accelerate/test_utils/__init__.py
+++ src/accelerate/test_utils/__init__.py
@@ -1,15 +1,23 @@
+from .other import (
+    backend_device_count,
+    backend_is_available,
+    torch_device,
+)
 from .testing import (
     are_the_same_tensors,
     assert_exception,
     execute_subprocess_async,
+    require_accelerator,
     require_bnb,
     require_cpu,
     require_cuda,
     require_huggingface_suite,
     require_mps,
+    require_multi_accelerator,
     require_multi_gpu,
     require_multi_xpu,
     require_safetensors,
+    require_single_accelerator,
     require_single_gpu,
     require_single_xpu,
     require_torch_min_version,
